f0e9b6b8b8635baf910c2d0f9ec09b6774e355ff
bpluta <bartekpluta@me.com>
2019-12-27 00:19:39 +0100

    Added fetching next commits from websocket

--- middleware/commit.py
+++ middleware/commit.py
@@ -19,21 +19,60 @@ class Commit:
 
         print("Instruction :: " + self.instruction.value)
 
+    def toDictionary(self):
+        registers = []
+        for index in range (0,len(self.registers)):
+            registers.append(self.registers[index].toDictionary())
+
+        memory = []
+        for index in range (0,len(self.memory)):
+            registers.append(self.memory[index].toDictionary())
+
+        return {
+            "index": self.index,
+            "registers": registers,
+            "memory": memory,
+            "instruction": self.instruction.toDictionary(),
+        }
+
 class RegisterCommit:
     def __init__(self, register, value):
         self.register = register
         self.value = value
+
+    def toDictionary(self):
+        return {
+            "register": self.register.name,
+            "value": hex(self.value)
+        }
 
 class MemoryCommit:
     def __init__(self, address, value):
         self.address = address
         self.value = value
 
+    def toDictionary(self):
+        return {
+            "address": hex(self.address),
+            "value": hex(self.value),
+        }
+
 class InstructionCommit:
     def __init__(self, value):
         self.value = value
+
+    def toDictionary(self):
+        return {
+            "value": self.value
+        }
 
 class CommitData:
     def __init__(self, index, value):
         self.index = index
         self.value = value
+
+    def toDictionary(self):
+        return {
+            "index": self.index,
+            "value": self.value.toDictionary()
+        }
--- middleware/server.py
+++ middleware/server.py
@@ -1,25 +1,60 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+
+from trace_handler import *
+from decoder import *
+
 import json
 
-app = Flask(__name__, static_folder="../frontend/dist", template_folder="../frontend")
-socketio = SocketIO(app)
+TRACE_FILE_PATH = "../traces/trace"
+
+server = Flask(__name__, static_folder="../frontend/dist", template_folder="../frontend")
+socketio = SocketIO(server)
+
+decoder = Decoder()
+tracer = TraceHandler(TRACE_FILE_PATH)
 
 def parseJSON(response):
     data = str(response).replace("\'", "\"")
     return json.loads(data)
 
-@app.route("/")
+def validate(json, keys):
+    for index in range(0,len(keys)):
+        if not keys[index] in json:
+            return False
+        elif json[keys[index]] == None:
+            return False
+    return True
+
+@server.route("/")
 def index():
     return render_template("index.html")
+    
+@socketio.on('commits')
+def getCommits(request):
+    data = parseJSON(request)
+    if not validate(data, ["index", "amount"]):
+        response = { 'error': 'missing values' }
+        socketio.emit('commits', str(response))
 
-@socketio.on('event')
-def handleEvent(data):
-    print(str(data))
-    json = parseJSON(data)
-    message = json.get("message", "None")
-    response = {'message': message}
-    socketio.emit('event', str(response))
+    index = data["index"]
+    amount = data["amount"]
+
+    commits = tracer.getNextCommits(amount,index)
+
+    commitsArray = []
+    for index in range(0,len(commits)):
+        commit = decoder.decodeCommit(commits[index])
+        commitsArray.append(commit.toDictionary())
+
+    hasMore = False
+    if len(commits):
+        index = commits[-1].index
+        if index:
+            hasMore = tracer.hasMoreAfter(index)
+
+    response = { "commits": commitsArray, 'hasMore': hasMore }
+    socketio.emit('commits', json.dumps(response))
 
 if __name__ == "__main__":
-    app.run()
+    server.run()
